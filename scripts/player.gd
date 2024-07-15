904c09740d887775c09268664f147be9fd5dec72
Aleksandar Horvat <108305750+aleksandarhorvat@users.noreply.github.com>
2024-07-15 15:39:57 +0200

    initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+const speed = 100
+
+func _physics_process(delta):
+	player_movement(delta)
+	
+func player_movement(delta):
+	if Input.is_action_just_pressed("ui_right"):
+		velocity.x = speed
+		velocity.y = 0
+	if Input.is_action_just_pressed("ui_left"):
+		velocity.x = -speed
+		velocity.y = 0
+	if Input.is_action_just_pressed("ui_down"):
+		velocity.y = speed
+		velocity.x = 0
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y = -speed
+		velocity.x = 0
+	else:
+		velocity.x = 0
+		velocity.y = 0
