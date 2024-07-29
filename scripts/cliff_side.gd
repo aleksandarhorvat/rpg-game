b44f8dd7e752061a22b9d1bf2f7ee3e2a77f6215
Aleksandar Horvat <108305750+aleksandarhorvat@users.noreply.github.com>
2024-07-29 16:06:16 +0200

    changed scene transition

--- scripts/cliff_side.gd
+++ scripts/cliff_side.gd
@@ -14,9 +14,9 @@ func _on_world_transition_point_body_entered(body):
 	if body.has_method("player"):
 		global.transition_scene = true
 
-func _on_world_transition_point_body_exited(body):
-	if body.has_method("player"):
-		global.transition_scene = false
+#func _on_world_transition_point_body_exited(body):
+	#if body.has_method("player"):
+		#global.transition_scene = false
 
 func change_scene():
 	if global.transition_scene == true:
--- scripts/world.gd
+++ scripts/world.gd
@@ -19,9 +19,9 @@ func _on_cliff_side_transition_point_body_entered(body):
 	if body.has_method("player"):
 		global.transition_scene = true
 
-func _on_cliff_side_transition_point_body_exited(body):
-	if body.has_method("player"):
-		global.transition_scene = false
+#func _on_cliff_side_transition_point_body_exited(body):
+	#if body.has_method("player"):
+		#global.transition_scene = false
 
 func change_scene():
 	if global.transition_scene == true:
